01eed972ef655896f98356926452a7d126ad389d
Vadim Pakhmurin <vp@vidium.org>
2019-08-22 23:32:27 +0300

    Initial commit

---
+++ CCU2VMIX/VMixWrapper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bosch2VMix
+{
+    class VMixWrapper
+    {
+        public VMixWrapper()
+        {
+
+        }
+    }
+}
